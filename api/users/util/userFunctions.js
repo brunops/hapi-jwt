8ee487c1887348206c65ed93dce49abc8c0f68e2
bsanches <bsanches@paypal.com>
2016-11-28 00:52:13 -0800

    Create /api/users/authenticate route

--- api/users/util/userFunctions.js
+++ api/users/util/userFunctions.js
@@ -2,6 +2,7 @@
 
 const Boom = require('boom')
 const User = require('../model/User')
+const bcrypt = require('bcrypt')
 
 const verifyUniqueUser = (req, res) => {
   // Find an entry from the database that
@@ -30,6 +31,32 @@ const verifyUniqueUser = (req, res) => {
   })
 }
 
+const verifyCredentials = (req, res) => {
+  const password = req.payload.password
+
+  // Find an entry from the database that
+  // matches either the email or the password
+  User.findOne({
+    $or: [
+      { username: req.payload.username },
+      { password: req.payload.password }
+    ],
+  }, (err, user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, (err, isValid) => {
+        if (!isValid) {
+          return res(Boom.badRequest('Incorrect password.'))
+        }
+
+        res(user)
+      })
+    } else {
+      res(Boom.badRequest('Incorrect username or password.'))
+    }
+  })
+}
+
 module.exports = {
-  verifyUniqueUser: verifyUniqueUser
+  verifyUniqueUser: verifyUniqueUser,
+  verifyCredentials: verifyCredentials
 }
